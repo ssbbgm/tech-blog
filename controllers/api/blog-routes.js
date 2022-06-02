d878d7ee4fbdcbf2f657a2024f93618eb453cffc
Sherreka <rekadarnb@gmail.com>
2022-06-02 09:53:35 -0400

    add  blog routes

---
+++ controllers/api/blog-routes.js
@@ -0,0 +1,126 @@
+const router = require('express').Router();
+const { Blog, User, Comment } = require('../../models');
+const sequelize = require('../../config/connection');
+const withAuth = require('../../utils/auth');
+
+router.get('/', (req, res) => {
+    console.log('======================');
+Blog.findAll({
+            attributes: ['id',
+                'title',
+                'body',
+                'created_at'
+            ],
+            order: [
+                ['created_at', 'DESC']
+            ],
+            include: [{
+                    model: User,
+                    attributes: ['username']
+                },
+                {
+                    model: Comment,
+                    attributes: ['id', 'body', 'blog_id', 'user_id', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                }
+            ]
+        })
+        .then(blogData => res.json(blogData.reverse()))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+
+});
+router.get('/:id', (req, res) => {
+    Blog.findOne({
+            where: {
+                id: req.params.id
+            },
+            attributes: ['id',
+                'body',
+                'title',
+                'created_at'
+            ],
+            include: [{
+                    model: User,
+                    attributes: ['username']
+                },
+                {
+                    model: Comment,
+                    attributes: ['id', 'body', 'blog_id', 'user_id', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                }
+            ]
+        })
+        .then(blogData => {
+            if (!blogData) {
+                res.status(404).json({ message: 'No blog found with this id' });
+                return;
+            }
+            res.json(blogData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.post('/', withAuth, (req, res) => {
+    Blog.create({
+            title: req.body.title,
+            content: req.body.body,
+            user_id: req.session.user_id
+        })
+        .then(blogData => res.json(blogData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.put('/:id', withAuth, (req, res) => {
+    Blog.update({
+            title: req.body.title,
+            content: req.body.body
+        }, {
+            where: {
+                id: req.params.id
+            }
+        }).then(blogData => {
+            if (!blogData) {
+                res.status(404).json({ message: 'No blog found with this id' });
+                return;
+            }
+            res.json(blogData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+router.delete('/:id', withAuth, (req, res) => {
+    Blog.destroy({
+        where: {
+            id: req.params.id
+        }
+    }).then(blogData => {
+        if (!blogData) {
+            res.status(404).json({ message: 'No blog found with this id' });
+            return;
+        }
+        res.json(blogData);
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
+
