a6beb4af11f16d026798f913d1581fd5f49ae596
Sherreka <rekadarnb@gmail.com>
2022-06-03 17:41:02 -0400

    added diffrent connection code

--- server.js
+++ server.js
@@ -35,6 +35,15 @@ app.use(express.static('public'));
 
 app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log(`Now listening on ${PORT}`));
-});
+// sequelize.sync({ force: false }).then(() => {
+//   app.listen(PORT, () => console.log(`Now listening on ${PORT}`));
+// });
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
